358a9324fdee7bd482031112d593d6bb230b8c1c
Ruiyu <ruiyuwu1998@gmail.com>
2019-09-25 20:42:55 -0400

    4.3

--- trees_graphs/problems.js
+++ trees_graphs/problems.js
@@ -31,4 +31,39 @@ function routeBetweenNodes(graph, node1, node2) {
     return false;
 }
 
-console.log(routeBetweenNodes(sample, "a", "c"))
+// console.log(routeBetweenNodes(sample, "a", "c"))
+
+// 4.3 List of Depths
+// Given a binary tree, design an algorithm which creates a linked list of all the nodes at each 
+// depth (e.g.if you have a tree with depth D, you'll have D linked lists).
+
+function listDepths(tree) {
+    const nodes = [];
+    const queue = [tree];
+    while (queue.length) {
+        nodes.push(queue.shift().value);
+        if (tree.left) {
+            queue.push(tree.left);
+        }
+        if (tree.right) queue.push(tree.right);
+    }
+
+    const lists = [];
+
+    for (let i = 0; i < depth; i++) {
+        const newNode = new Node(nodes.shift());
+        let node = newNode;
+        for (let j = 1; j < 2**i; j++) {
+            if (!nodes.length) {
+                break;
+            }
+            node.next = new Node(nodes.shift());
+        }
+        lists.push(newNode);
+        if (!nodes.length) {
+            break;
+        }
+    }
+
+    return lists;
+}
