7263d8c87f8c9c353ce6c52e5590c3f397f6d4e4
Ruiyu <ruiyuwu1998@gmail.com>
2019-10-10 00:00:19 -0400

    7.2

--- object_oriented_design/problems.js
+++ object_oriented_design/problems.js
@@ -77,3 +77,53 @@ class Blackjack {
     }
 }
 
+// 7.2 Call Center
+// Imagine you have a call center with three levels of employees: respondent, manager, and director. An incoming telephone
+// call must be first allocated to a respondent who is free. If the respondent can't handle the call, he or she must escalate
+// the call to a manager. If the manager is not free or not able to handle it, then the call should be escalated to a director.
+// Design the classes and data structures for this problem. Implement a method dispatchCall() which assigns a call to the first
+// available employee.
+
+// Assuming that employees can be freed up after a certain amount of time:
+
+class CallCenter {
+    constructor() {
+        this.respondents = [];
+        this.managers = [];
+        this.directors = [];
+    }
+
+    employ(person) {
+        this.employees.push(person);
+    }
+
+    dispatchCall() {
+        for (let i = 0; i < this.respondents.length; i++) {
+            if (this.respondents[i].free) this.respondents[i].free = false;
+            return `call taken by respondent ${name}`;
+        }
+
+        for (let i = 0; i < this.managers.length; i++) {
+            if (this.managers[i].free) this.managers[i].free = false;
+            return `call taken by manager ${name}`;
+        }
+
+        for (let i = 0; i < this.directors.length; i++) {
+            if (this.directors[i].free) this.directors[i].free = false;
+            return `call taken by director ${name}`;
+        }
+
+        return 'everyone is occupied';
+    }
+}
+
+class Employee {
+    constructor(name) {
+        this.name = name;
+        this.free = true;
+    }
+}
+
+class Respondent extends Employee { }
+class Manager extends Employee { }
+class Director extends Employee{}
